512bbf7658beb679b427d6ade678d723f60bb158
Renan Cascarano de Souza <30605188+canaldorenan2@users.noreply.github.com>
2018-03-28 15:26:26 -0300

    Add files via upload

---
+++ PHPexemplo.php
@@ -0,0 +1,14 @@
+<?php  
+ $boas_vindas = "Hellow World!!!"; 
+?>
+
+<html> 
+ <head> 
+  <title>Saudações do PHP</title> 
+ </head>
+ <body> 
+  <p>A saudação para a Turma de Programação para a Internet é:  
+   <?php echo $boas_vindas; ?>
+  </p> 
+ </body> 
+</html>
